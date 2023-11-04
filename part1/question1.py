c02cc55020a7c01e73ea53b263c89be643ad033e
EDMR <emermontes15@gmail.com>
2023-11-04 02:17:35 -0500

    Primer test pasando

--- part1/question1.py
+++ part1/question1.py
@@ -17,9 +17,11 @@ def get_city_temperature(city):
    if city == "Quito":
       return 22
    if city == "Sao Paulo":
-      return 17
+      return 14
    if city == "San Francisco":
       return 16
+   if city == "New York":
+      return 14
 
 def get_city_weather(city):
 
@@ -29,8 +31,6 @@ def get_city_weather(city):
      sky_condition = "cloudy"
   elif city == "New York":
      sky_condition = "rainy"
-  elif city == "Quito":
-     sky_condition = "sunny"
   elif city == "Quito":
      sky_condition = "sunny"
     

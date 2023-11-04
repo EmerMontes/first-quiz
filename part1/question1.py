ed965a8376e627b15436c65841b79dfd8e8d8c41
EDMR <emermontes15@gmail.com>
2023-11-03 22:58:51 -0500

    test 01 py

--- part1/question1.py
+++ part1/question1.py
@@ -29,6 +29,8 @@ def get_city_weather(city):
      sky_condition = "cloudy"
   elif city == "New York":
      sky_condition = "rainy"
+   elif city == "Quito":
+     sky_condition = "sunny"
 
   temperature = get_city_temperature(city)
 
